15bdd314cc2adfa6ffc1eb7ca4653e09215c4586
rahat-khan <rahat013166@gmail.com>
2023-07-17 22:05:36 +0600

    upload file

---
+++ Index.js
@@ -0,0 +1,19 @@
+
+// var numberOfWon = 0;
+// var numberOfLost = 0;
+// for(var i = 1; i <= 5; i++){
+//     var gessNamber = parseInt(prompt("Enter gess number 1 to 5 : "));
+
+//     var randomNumber = Math.floor(Math.random()*5)+1;
+    
+//     if(gessNamber==randomNumber){
+//         console.log("You have won");
+//         numberOfWon++;
+//     }
+//     else{
+//         console.log("You have lost. Random number was : " + randomNumber);
+//         numberOfLost++;
+//     }
+// }
+// document.write("Number of won : "+ numberOfWon + "<br>");
+// document.write("Number of lost : "+ numberOfLost + "<br>");
